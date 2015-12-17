ee26c4587463875b5384567d1df34e97b31236ba
chriss456 <Cott3@live.de>
2015-12-17 19:20:24 +0100

    Nom. neu:D

--- ProjectX/src/application/BetweenControler.java
+++ ProjectX/src/application/BetweenControler.java
@@ -9,28 +9,35 @@ import javafx.stage.Stage;
 public class BetweenControler {
 	
 	static int counter = 0;
+	static int inside_counter = 0;
 	
-	public void irgendwas(){
-		
-		if(counter == 2){
-			fikus_button_insert();
+	
+	public void fikus_button(){
+		if(counter == 6){
+			Stage primarystage = new Stage();
+			Fikus_Window window = new Fikus_Window();
+			window.start(primarystage);
 		}
-		
-		else if(counter == 6){
-			fikus_button_update();
+
+		else if(counter == 2){
+			Stage primarystage = new Stage();
+			Insert_fikus_window window = new Insert_fikus_window();
+			window.start(primarystage);
 		}
 	}
-	
-	public void fikus_button_update(){
 		
-		Stage primarystage = new Stage();
-		Fikus_Window window = new Fikus_Window();
-		window.start(primarystage);
-	}
-	
-	public void fikus_button_insert(){
-		Stage primarystage = new Stage();
-		Insert_fikus_window window = new Insert_fikus_window();
-		window.start(primarystage);
+	public void perkus_button(){
+		if(counter == 6){
+			Stage primarystage = new Stage();
+			Update_perkus_window window = new Update_perkus_window();
+			window.start(primarystage);
+		}
+		
+		else if(counter == 2){
+			Stage primarystage = new Stage();
+			Insert_perkus_window window = new Insert_perkus_window();
+			window.start(primarystage);
+		}
+
 	}
 }
