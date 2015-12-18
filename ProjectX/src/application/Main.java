50508b476fb08262bc3df087da3ca2b9cfa902e4
chriss456 <Cott3@live.de>
2015-12-18 10:16:18 +0100

    window update

--- ProjectX/src/application/Main.java
+++ ProjectX/src/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
-			Scene scene = new Scene(root, 500,500);
+			Scene scene = new Scene(root, 1200,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Start");
--- ProjectX/src/application/MainControler.java
+++ ProjectX/src/application/MainControler.java
@@ -7,40 +7,9 @@ public class MainControler {
 	Between_Window window;
 	Stage primaryStage = new Stage();
 	
-	public void search(){
+	public void Datenbank_modifizieren(){
 		BetweenControler.counter = 1;
 		window = new Between_Window();
 		window.start(primaryStage);
 	}
-	
-	public void insert(){
-		BetweenControler.counter = 2;
-		window = new Between_Window();
-		window.start(primaryStage);
-	}
-	
-	public void delete(){
-		BetweenControler.counter = 3;
-		window = new Between_Window();
-		window.start(primaryStage);
-	}
-	
-	public void create(){
-		BetweenControler.counter = 4;
-		window = new Between_Window();
-		window.start(primaryStage);
-	}
-	
-	public void show(){
-		BetweenControler.counter = 5;
-		window = new Between_Window();
-		window.start(primaryStage);
-	}
-	
-	public void update(){
-		BetweenControler.counter = 6;
-		window = new Between_Window();
-		window.start(primaryStage);
-	}
-
 }
