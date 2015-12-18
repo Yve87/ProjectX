df9c843cced98a5896087ff08b5df7aef411cb02
Yve87 <Elefant.01>
2015-12-18 10:08:19 +0100

    mainwindow first attempt

--- ProjectX/src/application/Main.java
+++ ProjectX/src/application/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 	
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("windowtest.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
 			Scene scene = new Scene(root, 500,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
