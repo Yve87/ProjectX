86ad9e9d9c0400967a9f8b9e7366d57841b0b9dc
Yve87 <Elefant.01>
2016-01-28 17:12:39 +0100

    perkus show window

--- ProjectX/src/application/perkus_show_window.java
+++ ProjectX/src/application/perkus_show_window.java
@@ -22,7 +22,7 @@ public class perkus_show_window extends Application {
 			scene = new Scene(root, 1200,700);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Perkus");
+			primaryStage.setTitle("Personenkunden");
 			primaryStage.show();
 			
 		} catch(Exception e) {
