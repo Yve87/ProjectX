42d2808201407f41046652450a8217e8168b2a53
Yve87 <Elefant.01>
2016-02-04 22:40:48 +0100

    idlieferschein

--- ProjectX/src/application/DeliveryNoteWindow_Controller.java
+++ ProjectX/src/application/DeliveryNoteWindow_Controller.java
@@ -269,7 +269,12 @@ public void postion_hinzufuegen() throws DocumentException{
 				lieferschein = new DeliveryNote(set.getInt(1),set.getDate(2),set.getString(3),
 				set.getString(4),set.getDouble(5),set.getFloat(6),set.getInt(7));
 			}
-			idlieferschein = lieferschein.getlieferscheinid();
+			
+			if(lieferschein.getlieferscheinid()==0) {
+				idlieferschein = 0;
+			} else {
+				idlieferschein = lieferschein.getlieferscheinid();
+			}
 			
 			String query1 = "SELECT Name FROM Produkt";
 			PreparedStatement stmt1 = conn.prepareStatement(query1);
